42c0e7f6159f8e291587b6f5d08d0194ec711e6f
AntoineDao <antoinedao1@gmail.com>
2020-02-26 13:49:31 +1000

    ci(semantic-release): update config and docs code to produce schema assets
    #14

--- docs.py
+++ docs.py
@@ -4,12 +4,26 @@ from dragonfly_schema.model import Model
 from honeybee_schema.energy.simulation import SimulationParameter
 
 import json
+import argparse
+
+parser =  argparse.ArgumentParser(description='Generate OpenAPI JSON schemas')
+
+parser.add_argument('--version',
+                    help='Set the version of the new OpenAPI Schema')
+
+args = parser.parse_args()
+
+VERSION = None
+
+if args.version:
+    VERSION = args.version.replace('v', '')
 
 # generate Model open api schema
 print('Generating Model documentation...')
 openapi = get_openapi(
     [Model],
     title='Dragonfly Model Schema',
-    description='This is the documentation for Dragonfly model schema.')
+    description='This is the documentation for Dragonfly model schema.',
+    version=VERSION)
 with open('./docs/model.json', 'w') as out_file:
     json.dump(openapi, out_file, indent=2)
