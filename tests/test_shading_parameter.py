bebcf172f55af554407c0b1c154e020b4dedd2cf
Mostapha <sadeghipour@gmail.com>
2020-04-20 14:09:42 -0400

    style(tests): remove double spaces

--- tests/test_shading_parameter.py
+++ tests/test_shading_parameter.py
@@ -9,20 +9,20 @@ target_folder = os.path.join(root, 'samples')
 
 
 def test_shading_par_extruded_border():
-    file_path  = os.path.join(target_folder, 'shading_par_extruded_border.json')
+    file_path = os.path.join(target_folder, 'shading_par_extruded_border.json')
     ExtrudedBorder.parse_file(file_path)
 
 
 def test_shading_par_overhang():
-    file_path  = os.path.join(target_folder, 'shading_par_overhang.json')
+    file_path = os.path.join(target_folder, 'shading_par_overhang.json')
     Overhang.parse_file(file_path)
 
 
 def test_shading_par_louvers_by_distance():
-    file_path  = os.path.join(target_folder, 'shading_par_louvers_by_distance.json')
+    file_path = os.path.join(target_folder, 'shading_par_louvers_by_distance.json')
     LouversByDistance.parse_file(file_path)
 
 
 def test_shading_par_louvers_by_count():
-    file_path  = os.path.join(target_folder, 'shading_par_louvers_by_count.json')
+    file_path = os.path.join(target_folder, 'shading_par_louvers_by_count.json')
     LouversByCount.parse_file(file_path)
