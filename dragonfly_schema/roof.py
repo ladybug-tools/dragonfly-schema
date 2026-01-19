716aecffc43c253c2bf5b81d7fd1176f44510a24
Chris Mackey <chris@ladybug.tools>
2026-01-19 14:38:45 -0800

    fix(roof): Add support for Mesh3D in RoofSpecification

--- dragonfly_schema/roof.py
+++ dragonfly_schema/roof.py
@@ -1,8 +1,9 @@
 """Geometry for specifying sloped roofs over a Story."""
 from pydantic import Field, constr, conlist
+from typing import Union
 
 from honeybee_schema._base import NoExtraBaseModel
-from honeybee_schema.model import Face3D
+from honeybee_schema.model import Face3D, Mesh3D
 
 
 class RoofSpecification(NoExtraBaseModel):
@@ -10,10 +11,10 @@ class RoofSpecification(NoExtraBaseModel):
 
     type: constr(regex='^RoofSpecification$') = 'RoofSpecification'
 
-    geometry: conlist(Face3D, min_items=1) = Field(
+    geometry: conlist(Union[Face3D, Mesh3D], min_items=1) = Field(
         ...,
-        description='An array of Face3D objects representing the geometry of the '
-        'Roof. None of these geometries should overlap in plan and, together, these '
-        'Face3D should either completely cover or skip each Room2D of the Story '
-        'to which the RoofSpecification is assigned.'
+        description='An array of Face3D (or Mesh3D) objects representing the '
+        'geometry of the Roof. Cases where Room2Ds are only partially covered '
+        'by these roof geometries will result in those portions of the Room2Ds '
+        'being extruded to their floor_to_ceiling_height.'
     )
